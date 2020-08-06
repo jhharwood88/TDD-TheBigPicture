b1be3f7a1dd5607ea793b61e88704943bc7620fa
Jon Harwood <jon.harwood@ibm.com>
2020-08-06 10:07:23 -0500

    finished the lesson

--- FizzBuzz-Start/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz-Start/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -6,6 +6,33 @@ namespace FizzBuzz.Tests
     [TestClass]
     public class FizzBuzzTests
     {
+        private FizzBuzzService _fizzbuzz
+        public FizzBuzzTests() => _fizzbuzz - new FizzBuzzService();
 
-    }
+        [TestMethod]
+        public void ShouldPrintNumber()
+        {
+            Assert.AreEqual("2", _fizzbuzz.Print(2));
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizz()
+        {
+            Assert.AreEqual("Fizz", _fizzbuzz.Print(3));
+        }
+
+        [TestMethod]
+        public void ShouldPrintBuzz()
+        {
+            Assert.AreEqual("Buzz", _fizzbuzz.Print(5));
+            Assert.AreEqual("Buzz", _fizzbuzz.Print(10));
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizzBuzz()
+        {
+            Assert.AreEqual("FizzBuzz", _fizzbuzz.Print(15));
+            Assert.AreEqual("FizzBuzz", _fizzbuzz.Print(30));
+        }
+    }
 }
